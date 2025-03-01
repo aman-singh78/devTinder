0ab6997732d9c3c48645933b556696ec42453620
mynewrepository <as4844275@gmail.com>
2025-02-28 22:02:08 -0800

    playing with routes

--- src/index.js
+++ src/index.js
@@ -2,13 +2,27 @@ const express=require("express");
 
 const app=express();
 
+
+
+app.get("/user/:userId",(req,res)=>{
+    console.log(req.params);
+    res.send({firstName:" aman", LastName:"thapa"});
+})
+
+app.post("/user",(req,res)=>{
+    console.log("save data to database");
+    res.send("data successfully saved to database");
+})
+
 app.use("/test",(req,res)=>{       //request handler
-    res.send("hello from the server")
+    res.send("hello from the server");
 });
 
-app.use("/hello",(req,res)=>{
-    res.send("hello hello");
+app.delete("/user",(req,res)=>{
+    res.send("deleted successfully");
 })
+
+
 
 app.listen(3000,()=>{
     console.log("server successfully listening on port : 3000");
